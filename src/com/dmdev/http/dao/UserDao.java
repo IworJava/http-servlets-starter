ec439d8af7de9e0d600d703ced0751578a0c4a3f
Iw <iwor.java@gmail.com>
2023-05-25 21:37:14 +0400

    added userDao.findAll()

--- src/com/dmdev/http/dao/UserDao.java
+++ src/com/dmdev/http/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.dmdev.http.dao;
 
+import com.dmdev.http.entity.Gender;
+import com.dmdev.http.entity.Role;
 import com.dmdev.http.entity.User;
 import com.dmdev.http.util.ConnectionManager;
 import lombok.AccessLevel;
@@ -11,12 +13,15 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDao implements Dao<Integer, User> {
     private static final UserDao INSTANCE = new UserDao();
+    private static final String FIND_ALL_SQL = "SELECT * FROM users;";
     private static final String SAVE_SQL = """
             INSERT INTO users(name, birthday, image, email, password, gender, role)
             VALUES (?, ?, ?, ?, ?, ?, ?);
@@ -27,9 +32,34 @@ public class UserDao implements Dao<Integer, User> {
             WHERE email = ?;
             """;
 
+    @SneakyThrows
     @Override
     public List<User> findAll() {
-        return null;
+        try (
+                Connection connection = ConnectionManager.open();
+                PreparedStatement statement = connection.prepareStatement(FIND_ALL_SQL)
+        ) {
+            List<User> users = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                users.add(buildUser(resultSet));
+            }
+            return users;
+        }
+    }
+
+    @SneakyThrows
+    private User buildUser(ResultSet resultSet) {
+        return User.builder()
+                .id(resultSet.getObject("id", Integer.class))
+                .name(resultSet.getObject("name", String.class))
+                .birthday(resultSet.getObject("birthday", LocalDate.class))
+                .image(resultSet.getObject("image", String.class))
+                .email(resultSet.getObject("email", String.class))
+                .password(resultSet.getObject("password", String.class))
+                .gender(Gender.valueOf(resultSet.getObject("gender", String.class)))
+                .role(Role.valueOf(resultSet.getObject("role", String.class)))
+                .build();
     }
 
     @Override
